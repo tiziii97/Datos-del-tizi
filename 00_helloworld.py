f8b607c5459ecbae5440c4090988631021e43f84
tiziii97 <tizianojoelcoricuenca@gmail.com>
2025-04-23 07:51:33 -0300

    00_helloworld.py
    Modifique el hola python por hola, visca el barça

--- 00_helloworld.py
+++ 00_helloworld.py
@@ -3,8 +3,8 @@
 ### Hola Mundo ###
 
 # Nuestro hola mundo en Python
-print("Hola Python")
-print('Hola Python')
+print("Hola, Visca El Barca")
+print('Hola, Visca El Barca')
 
 # Esto es un comentario
 
