c71354866f46fe9703c5adf0685f46e5db4e1010
tiziii97 <tizianojoelcoricuenca@gmail.com>
2025-06-04 08:00:03 -0300

    Update 03_strings.py
    Agregue un bonito detras de la palabra string

--- 03_strings.py
+++ 03_strings.py
@@ -2,20 +2,20 @@
 
 ### Strings ###
 
-my_string = "Mi String"
-my_other_string = 'Mi otro String'
+my_string = "Mi bonito String"
+my_other_string = 'Mi otro bonito String'
 
 print(len(my_string))
 print(len(my_other_string))
 print(my_string + " " + my_other_string)
 
-my_new_line_string = "Este es un String\ncon salto de línea"
+my_new_line_string = "Este es un bonito String\ncon salto de línea"
 print(my_new_line_string)
 
-my_tab_string = "\tEste es un String con tabulación"
+my_tab_string = "\tEste es un bonito String con tabulación"
 print(my_tab_string)
 
-my_scape_string = "\\tEste es un String \\n escapado"
+my_scape_string = "\\tEste es un bonito String \\n escapado"
 print(my_scape_string)
 
 # Formateo
